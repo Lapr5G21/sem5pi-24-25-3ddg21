28b0094cb2cd83fc468266e9020cdf42c2ff9db0
1221352 <118180485+1221352@users.noreply.github.com>
2024-10-21 10:04:01 +0100

    Little changes in Patient configuration

--- dddnetcore/Infraestructure/Patients/PatientEntityTypeConfiguration.cs
+++ dddnetcore/Infraestructure/Patients/PatientEntityTypeConfiguration.cs
@@ -8,55 +8,51 @@ namespace DDDSample1.Infrastructure.Patients
     {
         public void Configure(EntityTypeBuilder<Patient> builder)
         {
+            // MedicalRecordNumber is the primary key
             builder.HasKey(p => p.MedicalRecordNumber);
 
+            builder.Property(p => p.MedicalRecordNumber)
+                .HasConversion(mrn => mrn.AsString(), mrn => new PatientMedicalRecordNumber(mrn))
+                .IsRequired();
+
             builder.Property(p => p.FirstName)
-                .HasConversion(fn => fn.FirstName, fn => new PatientFirstName(fn)  // Converte de string para PatientFirstName
-                )
+                .HasConversion(fn => fn.FirstName, fn => new PatientFirstName(fn))
                 .IsRequired();
 
             builder.Property(p => p.LastName)
-                .HasConversion(ln => ln.LastName, ln => new PatientLastName(ln)  // Converte de string para PatientLastName
-                )
+                .HasConversion(ln => ln.LastName, ln => new PatientLastName(ln))
                 .IsRequired();
 
             builder.Property(p => p.FullName)
-                .HasConversion(fn => fn.FullName, fn => new PatientFullName(fn)  // Converte de string para PatientFullName
-                )
+                .HasConversion(fn => fn.FullName, fn => new PatientFullName(fn))
                 .IsRequired();
 
             builder.Property(p => p.BirthDate)
                 .HasConversion(
-                    bd => bd.BirthDateString, bd => new PatientBirthDate(bd)  // Converte para PatientBirthDate
+                    bd => bd.BirthDateString, bd => new PatientBirthDate(bd)
                 )
                 .IsRequired();
 
             builder.Property(p => p.Gender)
-                .HasConversion(g => g.GenderValue,  g => new PatientGender(g)  // Converte de string para PatientGender
-                )
+                .HasConversion(g => g.GenderValue, g => new PatientGender(g))
                 .IsRequired();
 
             builder.Property(p => p.Email)
                 .HasConversion(
-                    em => em.EmailString,
-                    em => new PatientEmail(em)  // Converte de string para PatientEmail
+                    em => em.EmailString, em => new PatientEmail(em)
                 )
                 .IsRequired();
 
             builder.Property(p => p.PhoneNumber)
                 .HasConversion(
-                    pn => pn.PhoneNumber,
-                    pn => new PatientPhoneNumber(pn)  // Converte de string para PatientPhoneNumber
+                    pn => pn.PhoneNumber, pn => new PatientPhoneNumber(pn)
                 )
                 .IsRequired();
 
-            // Configuração da propriedade EmergencyContact
             builder.Property(p => p.EmergencyContact)
-                .HasConversion(ec => ec.EmergencyContact, ec => new PatientEmergencyContact(ec)  // Converte de string para PatientEmergencyContact
-                )
+                .HasConversion(ec => ec.EmergencyContact, ec => new PatientEmergencyContact(ec))
                 .IsRequired();
 
-            // Configuração da propriedade Active
             builder.Property(p => p.Active)
                 .HasColumnName("IsActive")
                 .IsRequired();
