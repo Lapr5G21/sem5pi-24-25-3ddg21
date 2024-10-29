c4a48bff1aed07d89a331ee8cdc902b3ba63d913
Bruno Miguel Gomes Ribeiro <1221352@isep.ipp.pt>
2024-10-29 18:26:04 +0000

    home page started and implemented login with auth0

--- frontend/src/pages/homepage/homepage.component.ts
+++ frontend/src/pages/homepage/homepage.component.ts
@@ -1,10 +1,13 @@
-// home-page.component.ts
 import { Component } from '@angular/core';
 import { AuthService } from '@auth0/auth0-angular';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @Component({
   selector: 'app-home-page',
-  standalone: true, 
+  standalone: true,
+  imports: [MatButtonModule, MatCardModule],  
   templateUrl: './homepage.component.html',
   styleUrls: ['./homepage.component.scss']
 })
