128ca622b5c1cfea39bf68c09b5c59ae8a9aa428
1230430 <1230430@isep.ipp.pt>
2024-11-05 16:20:16 +0000

    Created routes and component for Creating Operation Requests #42

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { SidebarComponent } from './components/sidebar/sidebar.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { CreateOperationRequestComponent } from './components/doctorDashboard/operationRequests/create-operation-request/create-operation-request.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet,SidebarComponent],
+  imports: [RouterOutlet,SidebarComponent,CreateOperationRequestComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,8 +1,11 @@
 import { Routes } from '@angular/router';
 import { SidebarComponent } from './components/sidebar/sidebar.component';
 import { CreateOperationTypesComponent } from './components/adminDashboard/operationTypes/createOperationTypes/create-operation-types/create-operation-types.component';
+import { CreateOperationRequestComponent } from './components/doctorDashboard/operationRequests/create-operation-request/create-operation-request.component';
+
 
 export const routes: Routes = [
     { path: 'sidebar', component: SidebarComponent }, 
-    { path: 'operationtypes/create', component: CreateOperationTypesComponent }, 
+    { path: 'adminDashboard/operationtypes/create', component: CreateOperationTypesComponent }, 
+    { path: 'doctorDashboard/operationRequests/create', component: CreateOperationRequestComponent },
 ];
