9adcc12f1a842ab7bb8484efdac76e9dce47cb8a
1221352 <118180485+1221352@users.noreply.github.com>
2024-11-05 12:57:03 +0000

    Updates in Operation type service #46

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -3,9 +3,10 @@ import { AppComponent } from './app/app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { importProvidersFrom } from '@angular/core';
 import { DropdownModule } from 'primeng/dropdown';
+import { HttpClientModule } from '@angular/common/http';
 
 bootstrapApplication(AppComponent, {
   providers: [
-    importProvidersFrom(BrowserAnimationsModule,DropdownModule,BrowserModule)
+    importProvidersFrom(BrowserAnimationsModule,DropdownModule,BrowserModule,HttpClientModule)
   ]
 }).catch(err => console.error(err));
