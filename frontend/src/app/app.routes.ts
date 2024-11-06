a8cad1035204580e6b58706e58171e6271275624
1230430 <1230430@isep.ipp.pt>
2024-11-06 12:33:50 +0000

    Doctordashboard and create operationRequest UI #42

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,6 +1,6 @@
 import { Routes } from '@angular/router';
 import { SidebarComponent } from './components/sidebar/sidebar.component';
-import { CreateOperationRequestComponent } from './components/doctorDashboard/operationRequests/create-operation-request/create-operation-request.component';
+import { CreateOperationRequestsComponent } from './components/doctor-dashboard/operationRequests/create-operation-requests/create-operation-requests.component';
 import { CreateOperationTypesComponent } from './components/admin-dashboard/operationTypes/create-operation-types/create-operation-types.component';
 import { MenubarComponent } from './components/menubar/menubar.component';
 import { AdminDashboardComponent } from './components/admin-dashboard/admin-dashboard.component';
@@ -8,6 +8,6 @@ import { AdminDashboardComponent } from './components/admin-dashboard/admin-dash
 export const routes: Routes = [
     { path: 'sidebar', component: SidebarComponent }, 
     { path: 'adminDashboard/operationtypes/create', component: CreateOperationTypesComponent }, 
-    { path: 'doctorDashboard/operationRequests/create', component: CreateOperationRequestComponent },
+    { path: 'doctorDashboard/operationRequests/create', component: CreateOperationRequestsComponent },
     { path: 'adminDashboard/home', component: AdminDashboardComponent },
 ];
---
+++ frontend/src/app/components/doctor-dashboard/operationRequests/create-operation-requests/create-operation-requests.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-create-operation-requests',
+  standalone: true,
+  imports: [],
+  templateUrl: './create-operation-requests.component.html',
+  styleUrl: './create-operation-requests.component.scss'
+})
+export class CreateOperationRequestsComponent {
+
+}
