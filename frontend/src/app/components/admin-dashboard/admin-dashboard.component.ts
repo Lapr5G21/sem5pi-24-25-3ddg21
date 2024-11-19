202b3564af5e91dc3fa7cfd780497a04a58d17bc
1221352 <118180485+1221352@users.noreply.github.com>
2024-11-19 13:06:12 +0000

    teste commit

--- frontend/src/app/components/admin-dashboard/admin-dashboard.component.ts
+++ frontend/src/app/components/admin-dashboard/admin-dashboard.component.ts
@@ -194,7 +194,6 @@ export class AdminDashboardComponent implements OnInit {
   showHospitalModelComponent() {
     this.router.navigate(['adminDashboard/3DModule']);
   }
-  
   openProfileDialog() {
     console.log("Clicou em Profile");
     if (this.ProfileMenuComponent) {
