93e970768516d0a83e10dcc6f0d622abcfbce224
1230430 <1230430@isep.ipp.pt>
2024-11-19 23:48:23 +0000

    textures and placement finished.

--- frontend/src/app/3DVisualizationModule/maze.js
+++ frontend/src/app/3DVisualizationModule/maze.js
@@ -17,37 +17,65 @@ export default class Maze {
 
         this.bed  = null;
         this.patient = null;
+        this.door = null;
+        this.bench = null;
         this.loadedPatient = false;
         this.loadedBed = false;
+        this.loadedDoor = false;
+        this.loadedBench = false;
 
         this.onLoad = function (description) {
-            
 
             const loader = new GLTFLoader();
-
+        
+            // Promises para carregar os modelos
             const loadBedPromise = new Promise((resolve, reject) => {
-                loader.load(".\models\gltf\hospital_bed.glb", (glb) => {
-                    this.table = { object: glb.scene };
+                loader.load("./models/gltf/hospital_bed.glb", (glb) => {
+                    this.bed = { object: glb.scene };
                     this.loadedBed = true;
-                    console.log("Table loaded successfully:", this.bed);
+                    console.log("Bed loaded successfully:", this.bed);
                     resolve();
                 }, undefined, (error) => {
                     console.error(`Error loading bed model (${error}).`);
                     reject(error);
                 });
-            });
-
+            });
+        
             const loadPatientPromise = new Promise((resolve, reject) => {
-                loader.load(".\models\gltf\patient.glb", (glb) => { 
+                loader.load("./models/gltf/patient.glb", (glb) => {
                     this.patient = { object: glb.scene };
                     this.loadedPatient = true;
-                    console.log("patient loaded successfully:", this.patient);
+                    console.log("Patient loaded successfully:", this.patient);
                     resolve();
                 }, undefined, (error) => {
                     console.error(`Error loading patient model (${error}).`);
                     reject(error);
                 });
-                        });
+            });
+
+            const loadDoorPromise = new Promise((resolve, reject) => {
+                loader.load("./models/gltf/hospital_door.glb", (glb) => {
+                    this.door = { object: glb.scene };
+                    this.loadedDoor = true;
+                    console.log("Door loaded successfully:", this.door);
+                    resolve();
+                }, undefined, (error) => {
+                    console.error(`Error loading door model (${error}).`);
+                    reject(error);
+                });
+            });
+
+            const loadBenchPromise = new Promise((resolve, reject) => {
+                loader.load("./models/gltf/tandem_seating_-_hospital.glb", (glb) => {
+                    this.bench = { object: glb.scene };
+                    this.loadedBench = true;
+                    console.log("Bench loaded successfully:", this.bench);
+                    resolve();
+                }, undefined, (error) => {
+                    console.error(`Error loading bench model (${error}).`);
+                    reject(error);
+                });
+            });
             
             // Store the maze's map and size
             this.map = description.map;
@@ -56,8 +84,6 @@ export default class Maze {
             // Store the player's initial position and direction
             this.initialPosition = this.cellToCartesian(description.initialPosition);
             this.initialDirection = description.initialDirection;
-
-            // Store the maze's exit location
             this.exitLocation = this.cellToCartesian(description.exitLocation);
 
             // Create a group of objects
@@ -71,18 +97,17 @@ export default class Maze {
             
             this.wall = new Wall({ textureUrl: description.wallTextureUrl });
 
-            // Create a Bed
-            
 
-            // Create a Patient 
-
-
-            
+            Promise.all([loadBedPromise, loadPatientPromise, loadBenchPromise, loadDoorPromise])
+                .then(() => {
+                    console.log("All models loaded successfully!");
 
             // Build the maze
             let wallObject;
             for (let i = 0; i <= description.size.width; i++) { // In order to represent the eastmost walls, the map width is one column greater than the actual maze width
                 for (let j = 0; j <= description.size.height; j++) { // In order to represent the southmost walls, the map height is one row greater than the actual maze height
+                   
+                    const cellValue = description.map[j][i];
                     /*
                      * description.map[][] | North wall | West wall
                      * --------------------+------------+-----------
@@ -92,31 +117,70 @@ export default class Maze {
                      *          3          |    Yes     |    Yes
                      *          4          |           Bed
                      *          5          |         Pacient
+                     *          6          |           Door
+                     *          7          |           Bench
                      */
-                    if (description.map[j][i] == 2 || description.map[j][i] == 3) {
+                    if (cellValue == 2 || cellValue == 3) {
+                        // Adiciona paredes
                         wallObject = this.wall.object.clone();
                         wallObject.position.set(i - description.size.width / 2.0 + 0.5, 0.5, j - description.size.height / 2.0);
                         this.object.add(wallObject);
                     }
-                    if (description.map[j][i] == 1 || description.map[j][i] == 3) {
+                    if (cellValue == 1 || cellValue == 3) {
                         wallObject = this.wall.object.clone();
                         wallObject.rotateY(Math.PI / 2.0);
                         wallObject.position.set(i - description.size.width / 2.0, 0.5, j - description.size.height / 2.0 + 0.5);
                         this.object.add(wallObject);
+                    }
+                    if (cellValue == 4 && this.loadedBed) {
+                        const bedObject = this.bed.object.clone();
+                        bedObject.position.set(i - description.size.width / 2.0 + 0.2, 0.1, j - description.size.height / 2.0 + 0.2);
+                        bedObject.scale.set(0.003, 0.003, 0.003); // Adjust scale
+                        bedObject.rotateY( -(Math.PI / 2));
+                        bedObject.castShadow = true;
+                        bedObject.receiveShadow = true;
+                        this.object.add(bedObject);
+                    }
+                    if (cellValue == 5 && this.loadedPatient) {
+                        // Adiciona o paciente
+                        const patientObject = this.patient.object.clone();
+                        patientObject.position.set(i - description.size.width / 2.0 + 0.2, 0.1, j - description.size.height / 2.0 + 0.2);
+                        patientObject.scale.set(0.005, 0.005, 0.005); // Adjust scale
+                        patientObject.rotateY( -(Math.PI / 2));
+                        patientObject.castShadow = true;
+                        patientObject.receiveShadow = true;
+                        this.object.add(patientObject);
+                    }
+                    if (cellValue == 6 && this.loadedDoor) {
+                        // Adiciona o porta
+                        const doorObject = this.door.object.clone();
+                        doorObject.position.set(i - description.size.width / 2.0 + 0.5, 0.01, j - description.size.height / 2.0 );
+                        doorObject.scale.set(0.3, 0.4, 0.5); // Adjust scale
+                        doorObject.rotateY( -(Math.PI / 2));
+                        doorObject.castShadow = true;
+                        doorObject.receiveShadow = true;
+                        this.object.add(doorObject);
+                    }
+                    if (cellValue == 7 && this.loadedBench) {
+                        // Adiciona o banco
+                        const benchObject = this.bench.object.clone();
+                        benchObject.position.set(i - description.size.width / 2.0 - 0.1, 0.1, j - description.size.height / 2.0 - 0.8);
+                        benchObject.scale.set(0.3, 0.4, 0.5); // Adjust scale
+                        benchObject.rotateY( -(Math.PI / 2 * 1.95));
+                        benchObject.castShadow = true;
+                        benchObject.receiveShadow = true;
+                        this.object.add(benchObject);
                     }
 
-                    if (description.map[j][i] == 4) {
-                        wallObject = this.wall.object.clone();
-                        wallObject.rotateY(Math.PI / 2.0);
-                        wallObject.position.set(i - description.size.width / 2.0, 0.5, j - description.size.height / 2.0 + 0.5);
-                        this.object.add(wallObject);
-                    }
                 }
             }
 
             this.object.scale.set(this.scale.x, this.scale.y, this.scale.z);
             this.loaded = true;
-        }
+        }).catch((error) => {
+            console.error(`Error loading models (${error}).`);
+        });
+    };
 
         this.onProgress = function (url, xhr) {
             console.log("Resource '" + url + "' " + (100.0 * xhr.loaded / xhr.total).toFixed(0) + "% loaded.");
