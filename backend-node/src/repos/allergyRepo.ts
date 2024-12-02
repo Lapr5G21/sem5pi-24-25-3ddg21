abeda36d04b3176ebc6338f8d5e7431470fe3306
1230430 <1230430@isep.ipp.pt>
2024-12-02 15:49:32 +0000

    Allergy Repositorie implemented, functionality not tested yet. #80

---
+++ backend-node/src/repos/allergyRepo.ts
@@ -0,0 +1,73 @@
+import { Service, Inject } from 'typedi';
+
+import IRoleRepo from "../services/IRepos/IRoleRepo";
+import { Role } from "../domain/role";
+import { RoleId } from "../domain/roleId";
+import { RoleMap } from "../mappers/RoleMap";
+
+import { Document, FilterQuery, Model } from 'mongoose';
+import { IRolePersistence } from '../dataschema/IRolePersistence';
+import { IAllergyPersistence } from '../dataschema/IAllergyPersistence';
+import { Allergy } from '../domain/Allergies/Allergy';
+import { AllergyId } from '../domain/Allergies/allergyId';
+import { AllergyMap } from '../mappers/AllergyMap';
+import IAllergyRepo from '../services/IRepos/IAllergyRepo';
+
+@Service()
+export default class AllergyRepo implements IAllergyRepo {
+  private models: any;
+
+  constructor(
+    @Inject('allergySchema') private allergySchema : Model<IAllergyPersistence & Document>,
+  ) {}
+
+  private createBaseQuery (): any {
+    return {
+      where: {},
+    }
+  }
+
+  public async exists(allergy: Allergy): Promise<boolean> {
+    
+    const idX = allergy.id instanceof AllergyId ? (<AllergyId>allergy.id).toValue() : allergy.id;
+
+    const query = { domainId: idX}; 
+    const allergyDocument = await this.allergySchema.findOne( query as FilterQuery<IAllergyPersistence & Document>);
+
+    return !!allergyDocument === true;
+  }
+
+  public async save (allergy: Allergy): Promise<Allergy> {
+    const query = { domainId: allergy.id.toString()}; 
+
+    const allergyDocument = await this.allergySchema.findOne( query );
+
+    try {
+      if (allergyDocument === null ) {
+        const rawAllergy: any = AllergyMap.toPersistence(allergy);
+
+        const allergyCreated = await this.allergySchema.create(rawAllergy);
+
+        return AllergyMap.toDomain(allergyCreated);
+      } else {
+        allergyDocument.name = allergy.name;
+        await allergyDocument.save();
+
+        return allergy;
+      }
+    } catch (err) {
+      throw err;
+    }
+  }
+
+  public async findByDomainId (allergyId: AllergyId | string): Promise<Allergy> {
+    const query = { domainId: allergyId};
+    const allergyRecord = await this.allergySchema.findOne( query as FilterQuery<IAllergyPersistence & Document> );
+
+    if( allergyRecord != null) {
+      return AllergyMap.toDomain(allergyRecord);
+    }
+    else
+      return null;
+  }
+}
---
+++ backend-node/src/services/IRepos/IAllergyRepo.ts
@@ -0,0 +1,12 @@
+import { Repo } from "../../core/infra/Repo";
+import { Allergy } from "../../domain/Allergies/Allergy";
+import { AllergyId } from "../../domain/Allergies/allergyId";
+
+export default interface IAllergyRepo extends Repo<Allergy> {
+  save(allergy: Allergy): Promise<Allergy>;
+  findByDomainId (allergyId: AllergyId | string): Promise<Allergy>;
+    
+  //findByIds (rolesIds: RoleId[]): Promise<Role[]>;
+  //saveCollection (roles: Role[]): Promise<Role[]>;
+  //removeByRoleIds (roles: RoleId[]): Promise<any>
+}
